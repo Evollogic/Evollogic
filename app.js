1c0c74ac911f5bf975212454806e113ca2e07fc8
Evollogic <sodhupnvending@gmail.com>
2026-05-11 19:56:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // 🛡️ SEU IP REAL (Te leva pro Hub) 🛡️
-const ALLOWED_IP = "138.94.168.160"; 
+const ALLOWED_IP = "45.71.211.101"; 
 
 // ==========================================
 // 🎵 MOTOR DE ÁUDIO (SOM HACKER + ECO) 🎵
